847038ce2190aca4ef3ed74229100079f2d71a22
azimut <azimut.github@protonmail.com>
2021-01-06 03:12:33 -0300

    clang format and new .c files

--- 15.c
+++ 15.c
@@ -3,7 +3,9 @@
 int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
   GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_widget_show(win);
+  GtkWidget *btn = gtk_button_new_with_label("Close");
+  gtk_container_add(GTK_CONTAINER(win), btn);
+  gtk_widget_show_all(win);
   gtk_main(); // Main loop
   return 0;
 }
--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ CFLAGS=-Wall -Wextra -std=c11 -pedantic `pkg-config --cflags $(PKGS)`
 LDFLAGS=`pkg-config --libs $(PKGS)` -lm
 
 14:
+15:
+15v2:
+
 
 .PHONY: clear
 clear:
